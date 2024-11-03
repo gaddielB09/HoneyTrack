f0147ede0158bf2236a448a89b5dd575e4c00cff
gaddielB09 <gaddielbarrios444@gmail.com>
2024-11-02 17:49:00 -0700

    I deleted Proyecto directory

--- Proyecto/js/sidebar.js
+++
@@ -1,6 +0,0 @@
-document.getElementById('btn').addEventListener('click', function() {
-    const sidebar = document.querySelector('.sidebar');
-    sidebar.classList.toggle('active'); // Alterna la clase active
-});
-
-
