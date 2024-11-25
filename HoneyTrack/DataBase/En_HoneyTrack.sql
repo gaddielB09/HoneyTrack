7a865320f7fec69b6d14f3485c06f5b2cd193836
SKSKronosUTT <0323105967@ut-tijuana.edu.mx>
2024-11-25 00:37:16 -0800

    HI team

--- HoneyTrack/DataBase/En_HoneyTrack.sql
+++ HoneyTrack/DataBase/En_HoneyTrack.sql
@@ -208,6 +208,23 @@ DELIMITER ;
 /*!50003 SET character_set_client  = @saved_cs_client */ ;
 /*!50003 SET character_set_results = @saved_cs_results */ ;
 /*!50003 SET collation_connection  = @saved_col_connection */ ;
+/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
+/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
+/*!50003 SET @saved_col_connection = @@collation_connection */ ;
+/*!50003 SET character_set_client  = utf8mb4 */ ;
+/*!50003 SET character_set_results = utf8mb4 */ ;
+/*!50003 SET collation_connection  = utf8mb4_unicode_ci */ ;
+/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
+/*!50003 SET sql_mode              = 'IGNORE_SPACE,ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION' */ ;
+DELIMITER ;;
+/*!50003 CREATE*/ /*!50017 DEFINER=`cisco`@`localhost`*/ /*!50003 TRIGGER `calculateLocationVolume` BEFORE INSERT ON `LOCATION` FOR EACH ROW BEGIN
+    SET NEW.volume = NEW.length * NEW.height * NEW.width;
+END */;;
+DELIMITER ;
+/*!50003 SET sql_mode              = @saved_sql_mode */ ;
+/*!50003 SET character_set_client  = @saved_cs_client */ ;
+/*!50003 SET character_set_results = @saved_cs_results */ ;
+/*!50003 SET collation_connection  = @saved_col_connection */ ;
 
 --
 -- Table structure for table `LOT`
@@ -1363,11 +1380,13 @@ CREATE DEFINER=`cisco`@`localhost` PROCEDURE `insertLocation`(
 BEGIN
     DECLARE code1 VARCHAR(8);
     DECLARE codeDub TINYINT(1);
-    SET code1 = CONCAT(asile, side, bay, level, position);
+
+    SET code1 = CONCAT(aisle, side, bay, level, position);
     SET codeDub = (SELECT COUNT(*) FROM LOCATION WHERE code = code1);
+
     IF codeDub = 0 THEN
-        INSERT INTO LOCATION (code, containerName, aisle, side, bay, level, position, length, height, width, area)
-        VALUES (code1, container, aisle, side, bay, level, position, length, height, width, area);
+        INSERT INTO LOCATION (code, containerName, aisle, side, bay, level, position, capacity, length, height, width, area)
+        VALUES (code1, container, aisle, side, bay, level, position, 0, length, height, width, area);
         SET msg = "Location created successfully";
     ELSE
         SET msg = "Duplicated location";
@@ -1706,4 +1725,4 @@ DELIMITER ;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2024-11-24 10:30:59
+-- Dump completed on 2024-11-24 21:35:47
