d020b08e4dcea0721ba1080e2c354b136dd2adb5
gaddielB09 <gaddielbarrios444@gmail.com>
2024-11-26 22:57:39 -0800

    new messages in raw material file

--- HoneyTrack/html/rawMaterials.php
+++ HoneyTrack/html/rawMaterials.php
@@ -12,7 +12,9 @@
         <link rel="stylesheet" href="../css/sidebar2.css">
         <link rel="stylesheet" href="../css/colors.css">
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
-        <link rel="stylesheet" href="path/to/font-awesome/css/all.min.css">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet">
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"></script>
+<link rel="stylesheet" href="../css/toast.css">
     </head>
     <body>
     
@@ -100,7 +102,7 @@
                     <div id="format1" class="format" style="display: none;">
                         
                         <!-- users forms -->
-                        <form action="../php/insertRawMaterial.php" method="post">
+                        <form action="../php/insertRawMaterial.php" method="post" id="addUsers">
                             <h1 class="home">Add Raw Material</h1>
                             
                             <section class="data">
--- HoneyTrack/php/insertRawMaterial.php
+++ HoneyTrack/php/insertRawMaterial.php
@@ -1,10 +1,10 @@
 <?php
     require "../php/connection.php";
     $db = connectdb();
-    $msg = "";
+    $response = [];
 
     if ($_POST) {
-        //Se obtienen los datos de la materia prima
+        // Se obtienen los datos de la materia prima
         $code = $_POST["code"];
         $name = $_POST["name"];
         $description = $_POST["description"];
@@ -14,17 +14,30 @@
         $width = $_POST["width"];
         $weight = $_POST["weight"];
 
-        //Se inserta la nueva materia prima
+        // Se inserta la nueva materia prima
         $query = "CALL insertRawMaterial('$code','$name','$description','$cost','$length','$height','$width','$weight',@msg)";
-        $response = mysqli_query($db, $query);
+        $execute = mysqli_query($db, $query);
 
-        //Se recupera el mensaje
+        // Se recupera el mensaje
         $query = "SELECT @msg AS msg";
-        $response = mysqli_query($db, $query);
-        while ($row = mysqli_fetch_assoc($response)) {
-            //Se asigna el mensaje a una variable para desplegarla en pantalla
+        $msgResult = mysqli_query($db, $query);
+
+        // Asegurarse de que siempre se asigna un mensaje
+        if ($msgResult) {
+            $row = mysqli_fetch_assoc($msgResult);
             $msg = $row["msg"];
-            header("Location: ../html/rawMaterials.php?msg=$msg");
+            if ($msg == 'Raw Material created successfully') {
+                $response['status'] = 'success'; // El tipo de respuesta
+            } else {
+                $response['status'] = 'error';
+            }
+            $response['msg'] = $msg;
+        } else {
+            $response['status'] = 'error';
+            $response['msg'] = 'Failed to retrieve message from database';
         }
     }
+
+    // Se envía la respuesta en formato JSON
+    echo json_encode($response);
 ?>
